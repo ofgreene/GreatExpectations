4906ff707b374175ebbb0673367254155ba20aad
ofgreene <ofgreene@yahoo.com>
2022-11-04 22:39:51 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="A Game of Applications">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
